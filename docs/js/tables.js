a252f64f43122e79c91cfd16da211c5b11b2bb06
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2026-04-27 15:13:23 +0100

    Refactor DataTable initialization for all tables
    Updated DataTable initialization to target all tables and modified configuration options.

--- docs/js/tables.js
+++ docs/js/tables.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    // This targets all tables rendered in your markdown
-    $('article table').DataTable({
-        "paging": true,
-        "searching": true,
-        "info": true,
-        "pageLength": 25, // Number of rows to show by default
-        "order": []       // Disable initial sort so it matches your TSV order
+    $('table').DataTable({
+        "paging": true, 
+        "pageLength": 10,
+        "lengthMenu": [10, 25, 50, 100], 
+        "searching": true, 
+        "ordering": true, 
+        "dom": 'lfrtip' 
     });
 });
