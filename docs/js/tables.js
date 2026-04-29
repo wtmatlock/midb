d0f40524b974beae5093b535bec344dc65dfdbdf
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2026-04-29 19:39:55 +0100

    Update tables.js

--- docs/js/tables.js
+++ docs/js/tables.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tables = document.querySelectorAll("table");
+    const tables = document.querySelectorAll(".tsv-table");
 
-    tables.forEach((table) => {
-        if (!table.classList.contains("datatable-initialised")) {
-            $(table).DataTable({
-                pageLength: 10,
-                scrollX: true,
-                autoWidth: false
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js';
+    document.head.appendChild(script);
+
+    script.onload = () => {
+        tables.forEach((table) => {
+            const tsvFile = table.getAttribute("data-tsv");
+            
+            Papa.parse(tsvFile, {
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                delimiter: "\t", 
+                complete: function(results) {
+                    $(table).DataTable({
+                        data: results.data,
+                        columns: results.meta.fields.map(field => ({ title: field, data: field })),
+                        pageLength: 10,
+                        searching: true, 
+                        scrollX: true,
+                        autoWidth: false
+                    });
+                }
             });
-
-            table.classList.add("datatable-initialised");
-        }
-    });
+        });
+    };
 });
