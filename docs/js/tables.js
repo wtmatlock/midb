a7c691032921b9aa572a3d12c14ab74ffce7729e
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2026-04-29 14:57:32 +0100

    Update tables.js

--- docs/js/tables.js
+++ docs/js/tables.js
@@ -1,6 +1,7 @@
-function loadTable(file, tableId) {
+$(document).ready(function() {
+
     $.ajax({
-        url: file,
+        url: 'integronfinder_results_integrons.tsv',
         dataType: 'text',
         success: function(data) {
             const lines = data.trim().split('\n');
@@ -9,7 +10,7 @@ function loadTable(file, tableId) {
             const columns = headers.map(h => ({ title: h }));
             const rows = lines.slice(1).map(l => l.split('\t'));
 
-            $(tableId).DataTable({
+            $('#integron-table').DataTable({
                 data: rows,
                 columns: columns,
                 pageLength: 10,
@@ -19,7 +20,26 @@ function loadTable(file, tableId) {
             });
         }
     });
-}
 
-loadTable('/integronfinder_results_integrons.tsv', '#integron-table');
-loadTable('/bakta_annotations.tsv', '#gene-table');
+    $.ajax({
+        url: 'bakta_annotations.tsv',
+        dataType: 'text',
+        success: function(data) {
+            const lines = data.trim().split('\n');
+            const headers = lines[0].split('\t');
+
+            const columns = headers.map(h => ({ title: h }));
+            const rows = lines.slice(1).map(l => l.split('\t'));
+
+            $('#gene-table').DataTable({
+                data: rows,
+                columns: columns,
+                pageLength: 10,
+                deferRender: true,
+                scrollX: true,
+                autoWidth: false
+            });
+        }
+    });
+
+});
