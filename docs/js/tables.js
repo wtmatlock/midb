820982246f4f9725e6085b7a63fef389eef72431
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2026-04-29 19:47:52 +0100

    Update tables.js

--- docs/js/tables.js
+++ docs/js/tables.js
@@ -1,38 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js';
-    document.head.appendChild(script);
+$(document).ready(function () {
+    $(".tsv-table").each(function () {
+        const table = this;
+        const tsvFileName = $(table).data("tsv");
+        
+        const siteRoot = window.location.pathname.includes('/quickstart/') ? '../' : './';
+        const tsvPath = siteRoot + tsvFileName;
 
-    script.onload = () => {
-        const tables = document.querySelectorAll(".tsv-table");
-
-        tables.forEach((table) => {
-            const tsvFile = table.getAttribute("data-tsv");
-            
-            const filePath = `../${tsvFile}`;
-
-            Papa.parse(filePath, {
-                download: true,
-                header: true,
-                skipEmptyLines: true,
-                delimiter: "\t", 
-                complete: function(results) {
-                    $(table).DataTable({
-                        data: results.data,
-                        columns: results.meta.fields.map(field => ({ 
-                            title: field, 
-                            data: field 
-                        })),
-                        pageLength: 10,
-                        searching: true,
-                        scrollX: true,
-                        autoWidth: false
-                    });
-                },
-                error: function(err) {
-                    console.error("Error loading TSV:", tsvFile, err);
-                }
-            });
+        Papa.parse(tsvPath, {
+            download: true,
+            header: true,
+            skipEmptyLines: true,
+            delimiter: "\t",
+            complete: function (results) {
+                $(table).DataTable({
+                    data: results.data,
+                    columns: results.meta.fields.map(field => ({
+                        title: field,
+                        data: field
+                    })),
+                    pageLength: 10,
+                    lengthMenu: [10, 25, 50],
+                    searching: true,
+                    scrollX: true,
+                    autoWidth: false,
+                    dom: 'frtip'
+                });
+            },
+            error: function (err) {
+                console.error("Could not load TSV file:", tsvPath, err);
+            }
         });
-    };
+    });
 });
